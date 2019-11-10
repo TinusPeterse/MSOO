f8bc90eca40df2a667145c0cb8b6cb71edcac416
TinusPeterse <43200125+TinusPeterse@users.noreply.github.com>
2019-11-10 13:55:13 +0100

    Start

--- Program.cs
+++ Program.cs
@@ -6,39 +6,7 @@ using System.Threading.Tasks;
 
 namespace MSO
 {
-    class Program
+    class ShopUI
     {
-        
-        static void Main(string[] args)
-        {
-			bool done = false;
-            WinkelWagentje winkelWagentje = new WinkelWagentje();
-            winkelWagentje.productCataloges.HaalproductenUitDatabase();
-			winkelWagentje.productCataloges.BeschrijfAlleProducten();
-			while (!done)
-			{
-				string input = Console.ReadLine();
-				int amount;
-				if (int.TryParse(input, out amount))
-				{
-					winkelWagentje.Add(Convert.ToInt32(amount - 1));
-					Console.Clear();
-					winkelWagentje.productCataloges.BeschrijfAlleProducten();
-				}
-				else
-					switch (input)
-					{
-						case "d":
-							done = true;
-							winkelWagentje.Betalen();
-							break;
-						case "w":
-							winkelWagentje.PrintWagentje();
-							break;
-					}
-			}
-            Console.WriteLine("TheEndForNow");
-            Console.ReadLine();
-        }
     }
 }
