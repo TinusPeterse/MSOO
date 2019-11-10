83e5a23fc681011fdb7984ca105d5d7d09b7f1c5
niels <n.j.thomassen@students.uu.nl>
2019-11-10 11:57:38 +0100

    Added Paying

---
+++ Prijsberekenaar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSO
+{
+	class Prijsberekenaar
+	{
+		public bool Verzendkostentoegevoegd = false;
+		private double Verzendkosten = 4.95;
+		public double Berekenprijs(Dictionary<Product, int> dic, List<Product> list)
+		{
+			double Prijs = 0;
+			foreach(Product a in list)
+				Prijs += a.prijs * dic[a];
+			if (Verzendkostentoegevoegd) Prijs += Verzendkosten;
+			return Prijs;
+		}
+
+	}
+}
