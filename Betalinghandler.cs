3a8b696b71a7496c59c05bae7dcbc053dbb02416
niels <n.j.thomassen@students.uu.nl>
2019-11-10 12:31:06 +0100

    Betalinghandler

---
+++ Betalinghandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSO
+{
+	class Betalinghandler
+	{
+		public WinkelWagentje winkelWagentje;
+		public string Probeerbetaling(string setting)
+		{
+			if (setting == "success") return Betalinggelukt();
+			if (setting == "failure") return Betalingmislukt();
+			else return "Parse error";
+		}
+		private string Betalingmislukt()
+		{
+			return "faal";
+		}
+		private string Betalinggelukt()
+		{
+			return "success";
+		}
+	}
+}
